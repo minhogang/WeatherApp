816b3f26a010690dc4d4eb74c29fe306afe28f01
minhogang <minhogang@yahoo.com>
2018-11-10 23:54:07 +1000

    add comments

--- src/Main.java
+++ src/Main.java
@@ -3,24 +3,26 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import com.google.gson.*;
-import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args) throws Exception{
-
         HTTPRequest();
-
     }
 
     public static void HTTPRequest() throws Exception {
         String API_KEY = "de2fd82e32608db37b4964f69900f105";
+        //This is the URL to send the request to with the given API key.
         String url = "http://api.openweathermap.org/data/2.5/weather?lat=13&lon=144&units=imperial&APPID=" + API_KEY;
         URL obj = new URL(url);
+
         HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+        //Set request type and headers
         con.setRequestMethod("GET");
         con.setRequestProperty("User-Agent", "Mozilla/5.0");
+
         System.out.println("Sending request...");
+        //Send request and read buffered data
         BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
         String inputLine;
         StringBuffer response = new StringBuffer();
@@ -28,6 +30,7 @@ public class Main {
             response.append(inputLine);
         }
         in.close();
+        //Parse the returned JSON with GSON package
         JsonObject jsonObject = new JsonParser().parse(response.toString()).getAsJsonObject();
         JsonObject weatherSub = jsonObject.getAsJsonArray("weather").get(0).getAsJsonObject();
         JsonObject mainSub = jsonObject.getAsJsonObject("main");
@@ -38,8 +41,6 @@ public class Main {
         double temperature = mainSub.get("temp").getAsDouble();
         double humidity = mainSub.get("humidity").getAsDouble();
         double windSpeed = windSub.get("speed").getAsDouble();
-
     }
-
 }
 
