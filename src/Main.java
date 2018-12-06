4d809ce341b6330f94d6da9cde143c2c3d50d4eb
minhogang <minhogang@yahoo.com>
2018-12-06 23:52:23 +1000

    Add CSV exporting

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
 import java.awt.Color;
 import java.awt.GridLayout;
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Scanner;
+import au.com.bytecode.opencsv.CSVWriter;
 
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
@@ -26,12 +29,19 @@ import Thunderstorm.ThunderstormFrame;
 
 public class Main extends JFrame {
 	public static void main(String[] args) throws EmptyOrInvalidArguments, Exception {
-
 		String API_KEY = "de2fd82e32608db37b4964f69900f105";
-		HTTPRequestCurrent(API_KEY, false);
+		//Use the --export argument to export a 5-day weather forecast to a CSV file.
+		if (args.length >= 1) {
+			if (!args[0].equals("--export")) {
+				throw new EmptyOrInvalidArguments("You have entered invalid arguments: Valid arguments are: --export");
+			} else if (args[0].equals("--export")) {
+				exportToCSV(API_KEY);
+			}
+		}
+		HTTPRequest(API_KEY, false);
 		System.out.println("Would you like to know the weather condition right now?");
 		Scanner input = new Scanner(System.in);
-		if (input.next().equals("yes")) {HTTPRequestCurrent(API_KEY, true);}
+		if (input.next().equals("yes")) {HTTPRequest(API_KEY, true);}
 		input.close();
 	}
 
@@ -114,7 +124,7 @@ public class Main extends JFrame {
 		frame.setVisible(true);
 	}
 
-	private static void HTTPRequestCurrent(String API_KEY, boolean getCurrent) throws Exception {
+	private static void HTTPRequest(String API_KEY, boolean getCurrent) throws Exception {
 
 		//This is the URL to send the request to with the given API key.
 		String url;
@@ -140,5 +150,43 @@ public class Main extends JFrame {
 		JsonObject jsonObject = new JsonParser().parse(response.toString()).getAsJsonObject();
 		if (getCurrent) currentWeatherData(jsonObject);
 		else fiveDayForecast(jsonObject);
+	}
+	private static void exportToCSV(String API_KEY) throws Exception {
+		String url = "http://api.openweathermap.org/data/2.5/forecast?lat=13&lon=144&units=imperial&APPID=" + API_KEY;
+		URL obj = new URL(url);
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+		con.setRequestMethod("GET");
+		con.setRequestProperty("User-Agent", "Mozilla/5.0");
+		System.out.println("Exporting to CSV...");
+		BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+		while ((inputLine = in.readLine()) != null) {
+			response.append(inputLine);
+		}
+		in.close();
+
+		JsonObject jsonObject = new JsonParser().parse(response.toString()).getAsJsonObject();
+		JsonArray list = jsonObject.get("list").getAsJsonArray();
+		String fileName = "Weather_Forecast_Logs.csv";
+		//Write to CSV
+		File file = new File(fileName);
+		FileWriter outputFile = new FileWriter(file, true); //append: true; to append text at the end of the file
+		CSVWriter writer = new CSVWriter(outputFile);
+		String[] header = {"Date", "Time", "Temperature", "Weather", "Description", "Wind Speed", "Humidity"};
+		writer.writeNext(header);
+		for (JsonElement data: list) {
+			String title = data.getAsJsonObject().get("dt_txt").getAsString();
+			String[] datetime = title.split(" ");
+			String date = datetime[0];
+			String time = datetime[1];
+			String temperature = data.getAsJsonObject().getAsJsonObject("main").get("temp").getAsString();
+			String weather = data.getAsJsonObject().getAsJsonArray("weather").get(0).getAsJsonObject().get("main").getAsString();
+			String description = data.getAsJsonObject().getAsJsonArray("weather").get(0).getAsJsonObject().get("description").getAsString();
+			String windSpeed = data.getAsJsonObject().getAsJsonObject("wind").get("speed").getAsString();
+			String humidity = data.getAsJsonObject().getAsJsonObject("main").get("humidity").getAsString();
+			writer.writeNext(new String[]{date, time, temperature, weather, description, windSpeed, humidity}); //write a line to the CSV file
+		}
+		writer.close();
 	}
 }
